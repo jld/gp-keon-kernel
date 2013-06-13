65d58559d234b4e348abdf7dbec51e277225d182
Jed Davis <jld@mozilla.com>
2013-06-13 13:58:09 -0700

    perf: ARM: Implement perf_arch_fetch_caller_regs

--- arch/arm/include/asm/perf_event.h
+++ arch/arm/include/asm/perf_event.h
@@ -12,6 +12,8 @@
 #ifndef __ARM_PERF_EVENT_H__
 #define __ARM_PERF_EVENT_H__
 
+#include <asm/ptrace.h>
+
 /* ARM performance counters start from 1 (in the cp15 accesses) so use the
  * same indexes here for consistency. */
 #define PERF_EVENT_INDEX_OFFSET 1
@@ -37,5 +39,37 @@ armpmu_get_pmu_id(void);
 
 extern int
 armpmu_get_max_events(void);
+
+/*
+ * We shouldn't need fp unless CONFIG_FRAME_POINTER is set, but
+ * conditionalizing this is probably more effort than it's worth.
+ *
+ * We can't actually get the caller's registers here; the saved PC and
+ * SP values have to be consistent or else EHABI unwinding won't work,
+ * and the only way to find the matching SP for the return address is
+ * to unwind the current function.  So we save the current state
+ * instead.
+ *
+ * Note that Thumb1 can't store high registers directly.
+ */
+#define perf_arch_fetch_caller_regs(regs, ip)			\
+do {								\
+	__u32 _cpsr, _sp, _fp, _lr, _pc;			\
+	__asm__ __volatile__("mrs %[_cpsr], cpsr\n\t"		\
+			     "mov %[_pc], pc\n\t"		\
+			     "mov %[_lr], lr\n\t"		\
+			     "mov %[_sp], sp\n\t"		\
+			     "mov %[_fp], fp\n\t"		\
+			     : [_cpsr] "=r" (_cpsr),		\
+			       [_pc] "=r" (_pc),		\
+			       [_lr] "=r" (_lr),		\
+			       [_sp] "=r" (_sp),		\
+			       [_fp] "=r" (_fp));		\
+	(regs)->ARM_cpsr = _cpsr;				\
+	(regs)->ARM_pc = _pc;					\
+	(regs)->ARM_lr = _lr;					\
+	(regs)->ARM_sp = _sp;					\
+	(regs)->ARM_fp = _fp;					\
+} while (0)
 
 #endif /* __ARM_PERF_EVENT_H__ */
